6db38776dfea3d9ce7885b914bd1578f3c9755ee
R3qu3 <R3qu3@users.noreply.github.com>
2022-10-24 22:23:31 -0400

    Git bat 2

--- git.bat
+++ git.bat
@@ -1,4 +1,2 @@
-cd c://xampp/htdocs/sial
-set HOME=%USERPROFILE%
-git pull 
-pause
+cd Class/UKED201801FSF2-Class-Repository-FSF
+c:\Users\johnc\AppData\Local\Programs\Git\git-bash.exe -c 'git pull; exec sh'
